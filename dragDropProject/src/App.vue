fa580614572950f2a672da5c58ad19885d14f38d
huseynhasilci <huseynhasilci@outlook.com>
2023-02-13 00:13:28 +0300

    new codes added.

--- dragDropProject/src/App.vue
+++ dragDropProject/src/App.vue
@@ -1,30 +1,77 @@
 <script setup>
-  import { VueFlow  } from '@vue-flow/core'
-  import { ref } from 'vue';
-  
-  const elements = ref([
-    // Nodes
-    // An input node, specified by using `type: 'input'`
-    { id: '1', type: 'input', label: 'Node 1', position: { x: 250, y: 5 } },
+import { VueFlow, useVueFlow } from '@vue-flow/core'
+import { nextTick, watch } from 'vue'
+import Sidebar from './components/Sidebar.vue';
+import { Background, BackgroundVariant } from '@vue-flow/background'
+import { MiniMap } from '@vue-flow/minimap'
+let id = 0
+const getId = () => `dndnode_${id++}`
 
-    // Default nodes, you can omit `type: 'default'`
-    { id: '2', label: 'Node 2', position: { x: 100, y: 100 }, },
-    { id: '3', label: 'Node 3', position: { x: 400, y: 100 } },
+const { findNode, onConnect, nodes, edges, addEdges, addNodes, viewport, project, vueFlowRef } = useVueFlow({
+  nodes: [
+    {
+      id: '1',
+      type: 'input',
+      label: 'input node',
+      position: { x: 250, y: 25 },
+    },
+  ],
+})
 
-    // An output node, specified by using `type: 'output'`
-    { id: '4', type: 'output', label: 'Node 4', position: { x: 400, y: 200 } },
+const onDragOver = (event) => {
+  event.preventDefault()
 
-    // Edges
-    // Most basic edge, only consists of an id, source-id and target-id
-    { id: 'e1-3', source: '1', target: '3' },
+  if (event.dataTransfer) {
+    event.dataTransfer.dropEffect = 'move'
+  }
+}
 
-    // An animated edge
-    { id: 'e1-2', source: '1', target: '2', animated: true },
-  ])
+onConnect((params) => addEdges([params]))
+
+const onDrop = (event) => {
+  const type = event.dataTransfer?.getData('application/vueflow')
+
+  const { left, top } = vueFlowRef.value.getBoundingClientRect()
+
+  const position = project({
+    x: event.clientX - left,
+    y: event.clientY - top,
+  })
+
+  const newNode = {
+    id: getId(),
+    type,
+    position,
+    label: `${type} node`,
+  }
+
+  addNodes([newNode])
+
+  // align node position after drop, so it's centered to the mouse
+  nextTick(() => {
+    const node = findNode(newNode.id)
+    const stop = watch(
+      () => node.dimensions,
+      (dimensions) => {
+        if (dimensions.width > 0 && dimensions.height > 0) {
+          node.position = { x: node.position.x - node.dimensions.width / 2, y: node.position.y - node.dimensions.height / 2 }
+          stop()
+        }
+      },
+      { deep: true, flush: 'post' },
+    )
+  })
+}
 </script>
 
 <template>
-  <div style="height: 1000px">
-    <VueFlow v-model="elements" />
+  <div class="dndflow" @drop="onDrop">
+    <VueFlow @dragover="onDragOver">
+      <MiniMap />
+
+      <Background :variant="BackgroundVariant.Lines" />
+    </VueFlow>
+    
+    <Sidebar />
   </div>
 </template>
